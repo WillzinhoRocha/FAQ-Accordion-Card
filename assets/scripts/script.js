60ca915c6a77d8e10195308bd628f0072d7bf7ec
William Rocha <williamferreira313@gmail.com>
2021-04-30 20:32:12 -0300

    changed the structure html an layout

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,20 +1,3 @@
 "use strict";
 
-const answer = document.querySelector(".answer");
-const btnShowAnswer = document.querySelector(".btn-down");
-
-const showAnswer = function () {
-  answer.classList.remove("hidden");
-};
-
-const hideAnswer = function () {
-  answer.classList.add("hidden");
-};
-
-btnShowAnswer.addEventListener("click", function (e) {
-  if (answer.classList.contains("hidden")) {
-    showAnswer();
-  } else {
-    hideAnswer();
-  }
-});
+console.log("teste");
