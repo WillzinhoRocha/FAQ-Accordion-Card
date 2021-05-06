dce8fb3b6d50f892f088eb5649c4aec3fe6365c9
William Rocha <williamferreira313@gmail.com>
2021-05-06 15:29:54 -0300

    Did the accordion logical with javascript

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,43 @@
 "use strict";
+
+document.querySelectorAll(".accordion-question").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    console.log("click");
+    let accCollapse = item.nextElementSibling;
+
+    if (!item.classList.contains("collapsing")) {
+      //Open accordion item
+      if (!item.classList.contains("open")) {
+        accCollapse.style.display = "block";
+        let accHeight = accCollapse.clientHeight;
+
+        setTimeout(() => {
+          accCollapse.style.height = accHeight + "px";
+          accCollapse.style.display = "";
+        }, 1);
+
+        accCollapse.classList = "accordion-collapse collapsing";
+
+        setTimeout(() => {
+          accCollapse.classList = "accordion-collapse open";
+        }, 300);
+      }
+      //Closse accordion item
+      else {
+        //Remove "collapse open" from .accordion-collapse, add "collapsing"
+        accCollapse.classList = "accordion-collapse collapsing";
+
+        setTimeout(() => {
+          accCollapse.style.height = "0px";
+        }, 1);
+        // After X amount of time, remove "collapsing" class and add "collapse"
+        setTimeout(() => {
+          accCollapse.classList = "accordion-collapse collapse";
+          accCollapse.style.height = "";
+        }, 300);
+      }
+
+      item.classList.toggle("open");
+    }
+  });
+});
